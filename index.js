ac2dbb236e06f65f9efebaaf13c6e43e9e4c6826
ErickDaniCB <erickdani10@hotmail.com>
2023-10-28 18:07:13 -0600

    Add data processing and HTML inputs

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-//Connect to the API 
-async function getData (location) { 
-  const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=18ca9c2c7872437398a211815231910&q=${location}&aqi=yes`);
+//Connect to the API
+async function getData(location) {
+  try {
+    const response = await fetch(
+      `http://api.weatherapi.com/v1/forecast.json?key=18ca9c2c7872437398a211815231910&q=${location}&days=3&aqi=no&alerts=no`
+    );
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-  const json = await response.json();
-  console.log(json);
+async function processData (location){
+  const data = await getData(location);
+  console.log(data);
+  return {
+    temperatureC: data.current.temp_c,
+    temperatureF: data.current.temp_f,
+    humidity: data.current.humidity,
+    condition: data.current.condition.text
+  }
 };
 
-getData('Madrid');
+async function displayData (location){
+  const data = await processData(location);
+  
+}
